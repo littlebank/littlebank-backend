7236e63c7855cee2012d79ef149929bdb7f08f92
xogus3492 <wkdxogus3492@gmail.com>
2025-04-16 14:56:46 +0900

    feat: 로그인 API 개발

---
+++ src/main/java/com/littlebank/finance/domain/user/dto/request/LoginRequest.java
@@ -0,0 +1,19 @@
+package com.littlebank.finance.domain.user.dto.request;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+
+@Getter
+public class LoginRequest {
+
+    @Email
+    @NotBlank
+    @Schema(description = "이메일", example = "example@gmail.com")
+    private String email;
+
+    @NotBlank
+    @Schema(description = "비밀번호", example = "password")
+    private String password;
+}
---
+++ src/main/java/com/littlebank/finance/global/jwt/TokenProvider.java
@@ -0,0 +1,108 @@
+package com.littlebank.finance.global.jwt;
+
+import com.littlebank.finance.global.security.CustomUserDetailsService;
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Date;
+import java.util.stream.Collectors;
+
+@Slf4j
+@Component
+public class TokenProvider implements InitializingBean {
+
+    private static final String AUTHORITIES_KEY = "authority";
+    private final String secret;
+    private final long accessTokenValidityInSeconds;
+    private final long refreshTokenValidityInSeconds;
+    private final CustomUserDetailsService customUserDetailsService;
+    private Key key;
+
+    public TokenProvider(
+            @Value("${jwt.secret}") String secret,
+            @Value("${jwt.accessToken-validity-in-seconds}") long accessTokenValidityInSeconds,
+            @Value("${jwt.refreshToken-validity-in-seconds}") long refreshTokenValidityInSeconds,
+            CustomUserDetailsService customUserDetailsService
+    ) {
+        this.secret = secret;
+        this.accessTokenValidityInSeconds = accessTokenValidityInSeconds * 1000;
+        this.refreshTokenValidityInSeconds = refreshTokenValidityInSeconds * 1000;
+        this.customUserDetailsService = customUserDetailsService;
+    }
+
+    @Override
+    public void afterPropertiesSet() {
+        byte[] keyBytes = Decoders.BASE64.decode(secret);
+        this.key = Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public String provideAccessToken(Authentication authentication) {
+        return createToken(authentication, this.accessTokenValidityInSeconds);
+    }
+
+    public String provideRefreshToken(Authentication authentication) {
+        return createToken(authentication, this.refreshTokenValidityInSeconds);
+    }
+
+    private String createToken(Authentication authentication, long validity) {
+        String authorities = authentication.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.joining(","));
+
+        long now = new Date().getTime();
+
+        return Jwts.builder()
+                .claim("email", authentication.getName())
+                .claim(AUTHORITIES_KEY, authorities)
+                .signWith(key, SignatureAlgorithm.HS512)
+                .setExpiration(new Date(now + validity))
+                .compact();
+    }
+
+    public Authentication getAuthentication(String token) {
+        Claims claims = Jwts
+                .parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+
+        Collection<? extends GrantedAuthority> authorities =
+                Arrays.stream(claims.get(AUTHORITIES_KEY).toString().split(","))
+                        .map(SimpleGrantedAuthority::new)
+                        .collect(Collectors.toList());
+
+        UserDetails userDetails = customUserDetailsService.loadUserByUsername((String) claims.get("email"));
+
+        return new UsernamePasswordAuthenticationToken(userDetails, userDetails.getPassword(), authorities);
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
+            return true;
+        } catch (io.jsonwebtoken.security.SecurityException | MalformedJwtException e) {
+            log.info("잘못된 JWT 서명입니다.");
+        } catch (ExpiredJwtException e) {
+            log.info("만료된 JWT 토큰입니다.");
+        } catch (UnsupportedJwtException e) {
+            log.info("지원되지 않는 JWT 토큰입니다.");
+        } catch (IllegalArgumentException e) {
+            log.info("JWT 토큰이 잘못되었습니다.");
+        }
+        return false;
+    }
+}
