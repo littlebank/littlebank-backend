53c62e7558844634126ca3cb5123428f382243d7
kimeunsur <124233661+kimeunsur@users.noreply.github.com>
2025-04-23 16:00:23 +0900

    fix: dto 스키마 추가

--- src/main/java/com/littlebank/finance/domain/chat/dto/request/ChatRoomRequest.java
+++ src/main/java/com/littlebank/finance/domain/chat/dto/request/ChatRoomRequest.java
@@ -1,5 +1,8 @@
 package com.littlebank.finance.domain.chat.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,8 +13,14 @@ import java.util.List;
 @Getter
 @Setter
 @NoArgsConstructor
+@Schema(description = "채팅방 생성 요청 DTO")
 public class ChatRoomRequest {
+
+    @NotNull
+    @Schema(description = "채팅방 이름", example = "ltbk 채팅방", required = true)
     private String name;
+
     @Size(min=2, message="최소 두 명 이상의 참여자가 필요합니다.")
+    @Schema(description = "참여자 사용자 ID 목록", example = "[1, 2]", required = true)
     private List<Long> participantIds;
 }
