7440357b91a9fdf2e48d3475d4b8d136007a91af
xogus3492 <wkdxogus3492@gmail.com>
2025-04-28 09:38:22 +0900

    fix: 이메일 확인용 메일 발신 API 메일 폼 불러오기 에러 해결
    운영체제 경로로 파일 찾는 방식 -> classpath에서 파일 찾는 방식

--- src/main/java/com/littlebank/finance/global/util/FileUtil.java
+++ src/main/java/com/littlebank/finance/global/util/FileUtil.java
@@ -6,7 +6,6 @@ import org.springframework.core.io.ClassPathResource;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class FileUtil {
@@ -14,7 +13,7 @@ public class FileUtil {
     public static String readFileFromResource(String path) {
         try {
             ClassPathResource resource = new ClassPathResource(path);
-            byte[] bytes = Files.readAllBytes(resource.getFile().toPath());
+            byte[] bytes = resource.getInputStream().readAllBytes();
             return new String(bytes, StandardCharsets.UTF_8);
         } catch (IOException e) {
             throw new RuntimeException("파일을 읽을 수 없습니다: " + path, e);
