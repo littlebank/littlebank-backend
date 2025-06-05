614ee93ac8e7f0f270dd4f1dbb526f3dd8607c4c
xogus3492 <wkdxogus3492@gmail.com>
2025-06-05 15:20:06 +0900

    feat: 포인트 환전 대기 중인 목록 조회 API 개발

--- src/main/java/com/littlebank/finance/domain/point/domain/repository/CustomRefundRepository.java
+++ src/main/java/com/littlebank/finance/domain/point/domain/repository/CustomRefundRepository.java
@@ -1,9 +1,13 @@
 package com.littlebank.finance.domain.point.domain.repository;
 
 import com.littlebank.finance.domain.point.dto.response.SendPointHistoryResponse;
+import com.littlebank.finance.domain.point.dto.response.WaitStatusRefundResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
 public interface CustomRefundRepository {
+    Page<WaitStatusRefundResponse> findRefundHistoryByUserId(Long userId, Pageable pageable);
     List<SendPointHistoryResponse> findRefundHistoryByUserId(Long userId);
 }
