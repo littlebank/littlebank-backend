d82b1b6bc8e28aae8fe9c69a33b5956febd0013c
kimeunsur <124233661+kimeunsur@users.noreply.github.com>
2025-06-07 19:47:05 +0900

    feat: (부모)목표 확인 도장 찍기 API 알림 완료

--- src/main/java/com/littlebank/finance/domain/goal/service/GoalService.java
+++ src/main/java/com/littlebank/finance/domain/goal/service/GoalService.java
@@ -173,6 +173,19 @@ public class GoalService {
             goal.achieve();
         }
 
+        // 알림
+        try {
+            Notification notification = notificationRepository.save(Notification.builder()
+                    .receiver(goal.getCreatedBy())
+                    .message("우리 부모님(" + ")이 목표를 승낙했습니다!") //부모 중 누구인지 추가하기
+                    .type(NotificationType.GOAL_STAMP)
+                    .targetId(goal.getId())
+                    .isRead(false)
+                    .build());
+            firebaseService.sendNotification(notification);
+        } catch (DataIntegrityViolationException e) {
+            log.warn("이미 동일한 알림이 존재합니다.");
+        }
         return CommonGoalResponse.of(goal);
     }
 
