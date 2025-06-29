e360255a138310235c5488e2cbad0f7e4ac0b56f
xogus3492 <wkdxogus3492@gmail.com>
2025-06-30 00:19:32 +0900

    feat: 목표 신청 파이어베이스 푸쉬 알림 로직 수정 및 메서드 추가

--- src/main/java/com/littlebank/finance/global/firebase/FirebaseService.java
+++ src/main/java/com/littlebank/finance/global/firebase/FirebaseService.java
@@ -4,6 +4,7 @@ import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.FirebaseMessagingException;
 import com.google.firebase.messaging.Message;
 import com.littlebank.finance.domain.notification.domain.Notification;
+import com.littlebank.finance.global.firebase.record.NotificationToSend;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
@@ -11,7 +12,7 @@ import org.springframework.stereotype.Service;
 @Slf4j
 @Service
 public class FirebaseService {
-    private final static String NOTIFICATION_IMAGE_URL = "https://avatars.githubusercontent.com/u/203592524?s=96&v=4"; // 임시
+    private final static String NOTIFICATION_IMAGE_URL = "";
 
     @Async
     public void sendNotification(Notification notification) {
@@ -21,6 +22,26 @@ public class FirebaseService {
                         com.google.firebase.messaging.Notification.builder()
                                 .setTitle(notification.getMessage())
                                 .setBody(notification.getSubMessage())
+                                .setImage(NOTIFICATION_IMAGE_URL)
+                                .build()
+                )
+                .build();
+        try {
+            String response = FirebaseMessaging.getInstance().send(message);
+            log.info("FCM 전송 성공! 메시지 ID: " + response);
+        } catch (FirebaseMessagingException e) {
+            log.error("FCM 전송 실패: " + e.getMessage());
+        }
+    }
+
+    @Async
+    public void sendNotification(NotificationToSend notification) {
+        Message message = Message.builder()
+                .setToken(notification.fcmToken())
+                .setNotification(
+                        com.google.firebase.messaging.Notification.builder()
+                                .setTitle(notification.message())
+                                .setBody(notification.subMessage())
                                 .setImage(NOTIFICATION_IMAGE_URL)
                                 .build()
                 )
