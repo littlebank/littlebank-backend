1517ce0193d9703e4b5cda002fdfabafe2fc03ef
xogus3492 <wkdxogus3492@gmail.com>
2025-07-12 23:07:23 +0900

    fix: 결제 승인 API 오류 해결1 + 로깅 추가

--- src/main/java/com/littlebank/finance/domain/point/domain/constant/TossPaymentMethod.java
+++ src/main/java/com/littlebank/finance/domain/point/domain/constant/TossPaymentMethod.java
@@ -1,5 +1,9 @@
 package com.littlebank.finance.domain.point.domain.constant;
 
 public enum TossPaymentMethod {
-    카드, 가상계좌, 간편결제, 휴대폰, 계좌이체
+    CARD,
+    VIRTUAL_ACCOUNT,
+    EASY_PAYMENT,
+    MOBILE_PHONE,
+    ACCOUNT_TRANSFER
 }
