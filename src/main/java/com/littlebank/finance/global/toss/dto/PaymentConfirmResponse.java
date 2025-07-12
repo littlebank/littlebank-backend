d6968440cf3b4add72ed5442765bd777cae1ed42
xogus3492 <wkdxogus3492@gmail.com>
2025-07-13 00:59:14 +0900

    fix: 결제 승인 API 오류 수정4 (@ToString 추가 로깅 목적)

--- src/main/java/com/littlebank/finance/global/toss/dto/PaymentConfirmResponse.java
+++ src/main/java/com/littlebank/finance/global/toss/dto/PaymentConfirmResponse.java
@@ -1,16 +1,17 @@
 package com.littlebank.finance.global.toss.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.littlebank.finance.domain.point.domain.constant.TossPaymentMethod;
 import com.littlebank.finance.domain.point.domain.constant.TossPaymentStatus;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 import java.time.OffsetDateTime;
 
 @Getter
 @NoArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
+@ToString
 public class PaymentConfirmResponse {
     private String paymentKey;
     private String orderId;
